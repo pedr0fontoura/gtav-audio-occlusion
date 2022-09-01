15f49b330b5f208ab3eebda299b74516a7e99c3e
pedr0fontoura <me@pedrofontoura.io>
2022-09-01 12:24:53 -0300

    refactor(core/utils): improves type guards assertion

--- src/core/utils/typeGuards.ts
+++ src/core/utils/typeGuards.ts
@@ -1,49 +1,35 @@
 import * as XML from '../types/xml';
 
+const hasProperties = <T>(object: T, properties: (keyof T)[]): boolean => {
+  const keys = Object.keys(object);
+
+  return properties.every(property => keys.includes(property as string));
+};
+
 export const isXMLCMapData = (variableToCheck: any): variableToCheck is XML.Ymap => {
-  return !!(variableToCheck as XML.Ymap).CMapData;
+  return hasProperties(variableToCheck as XML.Ymap, ['CMapData']);
 };
 
 export const isXMLCEntityDef = (variableToCheck: any): variableToCheck is XML.CEntityDef => {
-  const XMLCEntityDef = variableToCheck as XML.CEntityDef;
-
-  return !!XMLCEntityDef.$.type && !!XMLCEntityDef.archetypeName && !!XMLCEntityDef.position;
+  return hasProperties(variableToCheck as XML.CEntityDef, ['archetypeName', 'position']);
 };
 
 export const isXMLCArchetypeDef = (variableToCheck: any): variableToCheck is XML.Archetype => {
-  const XMLArchetypeDef = variableToCheck as XML.Archetype;
-
-  return !!XMLArchetypeDef.$.type && !!XMLArchetypeDef.name && !!XMLArchetypeDef.flags;
+  return hasProperties(variableToCheck as XML.Archetype, ['bbMax', 'bbMin', 'bsCentre', 'flags', 'lodDist', 'name']);
 };
 
 export const isXMLCMloArchetypeDef = (variableToCheck: any): variableToCheck is XML.CMloArchetypeDef => {
-  const XMLCMloArchetypeDef = variableToCheck as XML.CMloArchetypeDef;
-
-  return (
-    isXMLCArchetypeDef(XMLCMloArchetypeDef) &&
-    !!XMLCMloArchetypeDef.entities &&
-    !!XMLCMloArchetypeDef.rooms &&
-    !!XMLCMloArchetypeDef.portals
-  );
+  return isXMLCArchetypeDef(variableToCheck) && variableToCheck.$.type === 'CMloArchetypeDef';
 };
 
 export const isXMLCMapTypes = (variableToCheck: any): variableToCheck is XML.Ytyp => {
-  return !!(variableToCheck as XML.Ytyp).CMapTypes;
+  return hasProperties(variableToCheck as XML.Ytyp, ['CMapTypes']);
 };
 
 export const isXMLCMloRoomDef = (variableToCheck: any): variableToCheck is XML.CMloRoomDef => {
-  const XMLCMloRoomDef = variableToCheck as XML.CMloRoomDef;
-
-  return !!XMLCMloRoomDef.name && !!XMLCMloRoomDef.portalCount;
+  return hasProperties(variableToCheck as XML.CMloRoomDef, ['name', 'portalCount']);
 };
 
 export const isXMLCMloPortalDef = (variableToCheck: any): variableToCheck is XML.CMloPortalDef => {
-  const XMLCMloPortalDef = variableToCheck as XML.CMloPortalDef;
-
-  return (
-    !!XMLCMloPortalDef.roomFrom &&
-    !!XMLCMloPortalDef.roomTo &&
-    !!XMLCMloPortalDef.attachedObjects &&
-    !!XMLCMloPortalDef.flags
-  );
+  return hasProperties(variableToCheck as XML.CMloPortalDef, ['roomFrom', 'roomTo', 'flags', 'attachedObjects']);
 };
