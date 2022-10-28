1320e06fef2e6fa16e6742fb54b56319e161d471
pedr0fontoura <me@pedrofontoura.io>
2022-10-28 20:48:32 -0300

    feat(electron/renderer): show portal index on Entities page

--- src/electron/renderer/features/entities/components/Entities/components/PortalInfoEntityList/index.tsx
+++ src/electron/renderer/features/entities/components/Entities/components/PortalInfoEntityList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { SerializedNaOcclusionPortalInfoMetadata } from '@/electron/common/types/naOcclusionInteriorMetadata';
 
@@ -11,34 +11,55 @@ type PortalInfoEntityListProps = {
 };
 
 export const PortalInfoEntityList = ({ data }: PortalInfoEntityListProps) => {
-  const entities = useMemo(() => {
-    return data.flatMap(portalInfo => portalInfo.portalEntityList);
-  }, [data]);
+  const renderEntities = (): React.ReactNode[] => {
+    const rows = [];
+
+    let renderedPortals = 0;
+
+    data.forEach((portalInfo, portalInfoIndex) => {
+      const { portalEntityList } = portalInfo;
+
+      if (!portalEntityList.length) return;
+
+      renderedPortals++;
+
+      const isEven = (renderedPortals + 1) % 2 === 0;
+      const rowSpan = portalEntityList.length;
+
+      portalEntityList.forEach((portalEntity, portalEntityIndex) => {
+        const { linkType, maxOcclusion, entityModelName, entityModelHashKey, isDoor, isGlass } = portalEntity;
+
+        const isFirstEntity = portalEntityIndex === 0;
+
+        rows.push(
+          <tr key={`${portalInfoIndex}:${portalEntityIndex}`} className={isEven ? 'even' : 'odd'}>
+            {isFirstEntity && <td rowSpan={rowSpan}>{portalInfoIndex}</td>}
+            <td>{entityModelName ?? entityModelHashKey}</td>
+            <td>{maxOcclusion}</td>
+            <td>{isDoor.toString()}</td>
+            <td>{isGlass.toString()}</td>
+          </tr>,
+        );
+      });
+    });
+
+    return rows;
+  };
 
   return (
     <Container>
       <Content>
-        <Table>
-          <tr>
-            <th>Model</th>
-            <th>Max occlusion</th>
-            <th>Is door</th>
-            <th>Is glass</th>
-            <th>Link type</th>
-          </tr>
-          {entities.map(portalEntity => {
-            const { linkType, maxOcclusion, entityModelName, entityModelHashKey, isDoor, isGlass } = portalEntity;
-
-            return (
-              <tr>
-                <td>{entityModelName ?? entityModelHashKey}</td>
-                <td>{maxOcclusion}</td>
-                <td>{isDoor.toString()}</td>
-                <td>{isGlass.toString()}</td>
-                <td>{linkType}</td>
-              </tr>
-            );
-          })}
+        <Table alternatedRowColors={false}>
+          <thead>
+            <tr>
+              <th>Portal</th>
+              <th>Model</th>
+              <th>Max occlusion</th>
+              <th>Is door</th>
+              <th>Is glass</th>
+            </tr>
+          </thead>
+          <tbody>{renderEntities()}</tbody>
         </Table>
       </Content>
     </Container>
