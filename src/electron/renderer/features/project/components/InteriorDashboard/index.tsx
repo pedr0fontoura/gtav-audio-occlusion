4d9e96bce6acb6e05fbe94d45f00cf4631932af4
pedr0fontoura <me@pedrofontoura.io>
2022-11-08 18:34:40 -0300

    refactor(electron/renderer): updates interior dashboard design

--- src/electron/renderer/features/project/components/InteriorDashboard/index.tsx
+++ src/electron/renderer/features/project/components/InteriorDashboard/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { FaFileDownload } from 'react-icons/fa';
 
 import { useInterior } from '@/electron/renderer/features/interior';
 
-import { TableContainer, Table } from '@/electron/renderer/components/Table';
-
-import { Container } from './styles';
+import { Container, Section, SectionHeader, Horizontal, Entry, Path, Button } from './styles';
 
 export const InteriorDashboard = (): JSX.Element => {
   const { interior } = useInterior();
@@ -17,50 +16,49 @@ export const InteriorDashboard = (): JSX.Element => {
 
   return (
     <Container>
-      <div>
-        <p>Source</p>
-        <TableContainer>
-          <Table>
-            <thead>
-              <tr>
-                <th>Type</th>
-                <th>Path</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>#map</td>
-                <td>{mapDataFilePath}</td>
-              </tr>
-              <tr>
-                <td>#typ</td>
-                <td>{mapTypesFilePath}</td>
-              </tr>
-            </tbody>
-          </Table>
-        </TableContainer>
-      </div>
-      <div>
-        <p>Generated</p>
-        <TableContainer>
-          <Table>
-            <thead>
-              <tr>
-                <th>Type</th>
-                <th>Path</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>naOcclusionInteriorMetadata</td>
-                <td>C:/User/Documents/filename.ymap.xml</td>
-                <td></td>
-              </tr>
-            </tbody>
-          </Table>
-        </TableContainer>
-      </div>
+      <Section>
+        <SectionHeader>
+          <h2>Map info</h2>
+        </SectionHeader>
+        <Horizontal>
+          <Entry>
+            <h3>Map data (#map)</h3>
+            <Path>{mapDataFilePath}</Path>
+          </Entry>
+          <Entry>
+            <h3>Map types (#typ)</h3>
+            <Path>{mapTypesFilePath}</Path>
+          </Entry>
+        </Horizontal>
+        <Entry>
+          <h3>Path</h3>
+          <Path>...</Path>
+        </Entry>
+      </Section>
+      <Section>
+        <SectionHeader>
+          <h2>naOcclusionInteriorMetadata</h2>
+          <Button>
+            <FaFileDownload size={18} />
+          </Button>
+        </SectionHeader>
+        <Entry>
+          <h3>Path</h3>
+          <Path>...</Path>
+        </Entry>
+      </Section>
+      <Section>
+        <SectionHeader>
+          <h2>Audio Game Data</h2>
+          <Button>
+            <FaFileDownload size={18} />
+          </Button>
+        </SectionHeader>
+        <Entry>
+          <h3>Path</h3>
+          <Path>...</Path>
+        </Entry>
+      </Section>
     </Container>
   );
 };
--- src/electron/renderer/features/project/components/InteriorDashboard/styles.ts
+++ src/electron/renderer/features/project/components/InteriorDashboard/styles.ts
@@ -1,11 +1,88 @@
 import styled from 'styled-components';
+import { lighten, darken } from 'polished';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 16px;
+  gap: 8px;
+
+  font-size: 0.9em;
+
+  border-radius: 8px;
+
+  background: ${({ theme }) => lighten(0.02, theme.colors.gray[900])};
+
+  h2 {
+    font-size: 1.2em;
+  }
+
+  h3 {
+    font-weight: 500;
+    font-size: 1em;
+
+    margin-bottom: 4px;
+  }
 
   p {
-    margin-bottom: 8px;
+    font-size: 0.9em;
+    color: ${({ theme }) => theme.colors.gray[200]};
   }
+`;
+
+export const Section = styled.section`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  gap: 16px;
+
+  padding: 16px;
+`;
+
+export const SectionHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const Button = styled.button`
+  width: 32px;
+  height: 32px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: none;
+
+  color: #fff;
+
+  background: none;
+
+  transition: 0.2s;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.rose[600]};
+  }
+`;
+
+export const Horizontal = styled.div`
+  width: 100%;
+
+  display: flex;
+  gap: 16px;
+`;
+
+export const Entry = styled.div`
+  flex: 1 1;
+  flex-direction: column;
+
+  min-width: 0;
+`;
+
+export const Path = styled.p`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `;
