48101ee73bde397ab437ad00dcee248c02a20cd8
pedr0fontoura <me@pedrofontoura.io>
2022-11-08 13:29:08 -0300

    fix(core): fixes parsing hex to string

--- src/core/utils/string.ts
+++ src/core/utils/string.ts
@@ -1,1 +1,9 @@
-export const parseHexToString = (value: number): string => `0x${value.toString(16).toUpperCase()}`;
+export const parseHexToString = (value: number): string => {
+  const stringifiedValue = value.toString(16);
+
+  const paddedStringifiedValue = stringifiedValue.padEnd(8, '0');
+
+  const upperCasePaddedStringifiedValue = paddedStringifiedValue.toUpperCase();
+
+  return `0x${upperCasePaddedStringifiedValue}`;
+};
