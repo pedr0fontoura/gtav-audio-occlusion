1d7b0693bcdea3a69889ffa3fffed795e6700155
pedr0fontoura <me@pedrofontoura.io>
2022-11-16 17:00:32 -0300

    fix: fixes main window loading on packaged application

--- src/electron/main/app.ts
+++ src/electron/main/app.ts
@@ -47,11 +47,7 @@ export class Application {
       },
     });
 
-    if (process.env.NODE_ENV === 'development') {
-      this.mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-    } else {
-      this.mainWindow.loadFile(MAIN_WINDOW_WEBPACK_ENTRY);
-    }
+    this.mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
     this.mainWindow.on('page-title-updated', e => {
       e.preventDefault();
